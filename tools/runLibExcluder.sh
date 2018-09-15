b583893dbe64c6a322e5bc3166fbd8ce33b33542
Li Li <lilicoding@gmail.com>
2018-09-15 19:01:19 +0200

    add tools

---
+++ tools/runLibExcluder.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+APK=$1
+ANDROID_JARS=$2
+
+java -jar LibExcluder.jar $APK $ANDROID_JARS
---
+++ tools/runLibRepresenter.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+APK=$1
+ANDROID_JARS=$2
+
+java -jar LibRepresenter.jar $APK $ANDROID_JARS
